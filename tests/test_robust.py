c4a7eb254003d5976e5126bc596db14c2364314e
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2022-01-17 13:27:13 +0000

    Add mirrors

---
+++ tests/test_robust.py
@@ -0,0 +1,58 @@
+# (C) Copyright 2021 ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+#
+
+import logging
+import signal
+from contextlib import contextmanager
+
+from multiurl import download
+from multiurl.http import RETRIABLE
+
+
+def handler(signum, frame):
+    raise TimeoutError()
+
+
+@contextmanager
+def timeout(s):
+    save = signal.signal(signal.SIGALRM, handler)
+    signal.alarm(s)
+    try:
+        yield
+    finally:
+        signal.alarm(0)
+        signal.signal(signal.SIGALRM, save)
+
+
+def test_robust():
+    sleep = 5
+    with timeout(len(RETRIABLE * sleep * 10)):
+        for code in RETRIABLE:
+            download(
+                f"http://httpbin.org/status/200,{code}",
+                retry_after=sleep,
+                target="test.data",
+            )
+
+
+# def test_mirror():
+
+#     download(
+#         "http://httpbin.org/status/500",
+#         mirrors={
+#             "http://httpbin.org/status/500": [
+#                 "http://download.ecmwf.int/test-data/metview/gallery/temp.bufr"
+#             ]
+#         },
+#     )
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    test_robust()
